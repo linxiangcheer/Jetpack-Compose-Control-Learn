1b22599d29f8ccf81396b861c24ee79d993cdd34
linxiang <1073456077@qq.com>
2021-07-18 19:14:38 +0800

    用padding来实验属性先后顺序的影响

--- app/src/main/java/com/linx/jetpack_compose_control_learn/fragment/chapter1_basics/ControlLearn1_1ColumnRowBoxScreen.kt
+++ app/src/main/java/com/linx/jetpack_compose_control_learn/fragment/chapter1_basics/ControlLearn1_1ColumnRowBoxScreen.kt
@@ -12,9 +12,7 @@ import androidx.compose.ui.unit.dp
 import com.linx.jetpack_compose_control_learn.components.ControlLearnDescription
 import com.linx.jetpack_compose_control_learn.components.ControlLearnExampleContentText
 import com.linx.jetpack_compose_control_learn.components.ControlLearnHeader
-import com.linx.jetpack_compose_control_learn.ui.theme.c_FF9800
-import com.linx.jetpack_compose_control_learn.ui.theme.c_FFA726
-import com.linx.jetpack_compose_control_learn.ui.theme.c_FFB74D
+import com.linx.jetpack_compose_control_learn.ui.theme.*
 
 /**
  * 关于[Column]、[Row]、[Box]和[Modifier]
@@ -26,7 +24,7 @@ import com.linx.jetpack_compose_control_learn.ui.theme.c_FFB74D
  * [Modifier]用于设置属性,如dimensions、padding、background color、click等
  * 注意：
  * Modifier中的属性设置顺序很重要,会影响到最后的效果.
- * 后设置的Modifier属性会先执行，例如Modifier.padding(1.dp).click{...}.padding(2.dp),点击事件不会在padding(1.dp)内触发
+ * 先写的属性会先执行,后面的属性会在前面的属性的基础上执行
  */
 @Composable
 fun ControlLearn1_1ColumnRowBoxScreen() {
@@ -42,9 +40,22 @@ fun ControlLearn1_1Content() {
     ) {
         item {
 
+            //Row和Arrangement.Horizontal
             ControlLearnHeader("Row")
             ControlLearnDescription("1-) Row是可组合的布局,内部以左右结构摆放子节点.")
             RowExample()
+
+            //Column和Arrangement.Vertical
+            ControlLearnHeader(text = "Column")
+            ControlLearnDescription(text = "2-) Column是可组合的布局,内部以上下结构摆放子节点")
+            ColumnExample()
+
+            //padding
+            ControlLearnDescription(text = "3-) 实验属性先后顺序的影响,例如padding的先后顺序决定了它是内边距还是外边距")
+            ControlLearnExampleContentText(text = "background之后设置padding - 内边距")
+            ControlLearnExampleContentText(text = "在background之前设置padding - 外边距")
+            ControlLearnExampleContentText(text = "先写的属性会先执行,后面的属性会在前面的属性的基础上执行")
+            ColumnAndRowPaddingExample()
 
         }
     }
@@ -121,6 +132,139 @@ fun RowTexts() {
             .background(c_FFB74D)
             .padding(4.dp)
     )
+}
+
+/**
+ * [Column]的例子
+ */
+@Composable
+fun ColumnExample() {
+
+    val modifier = Modifier
+        .padding(8.dp)
+        .fillMaxWidth()
+        .height(160.dp)
+        .background(Color.LightGray)
+
+    //顶部
+    ControlLearnExampleContentText(text = "Arrangement.Top (顶部)")
+    Column(modifier = modifier, verticalArrangement = Arrangement.Top) {
+        ColumnTexts()
+    }
+
+    //底部
+    ControlLearnExampleContentText(text = "Arrangement.Bottom (底部)")
+    Column(modifier, verticalArrangement = Arrangement.Bottom) {
+        ColumnTexts()
+    }
+
+    //纵向居中
+    ControlLearnExampleContentText(text = "Arrangement.Center (居中)")
+    Column(modifier, verticalArrangement = Arrangement.Center) {
+        ColumnTexts()
+    }
+
+
+    ControlLearnExampleContentText(text = "Arrangement.SpaceEvenly")
+    Column(modifier, verticalArrangement = Arrangement.SpaceEvenly) {
+        ColumnTexts()
+    }
+
+    ControlLearnExampleContentText(text = "Arrangement.SpaceAround")
+    Column(modifier, verticalArrangement = Arrangement.SpaceAround) {
+        ColumnTexts()
+    }
+
+    ControlLearnExampleContentText(text = "Arrangement.SpaceBetween")
+    Column(modifier, verticalArrangement = Arrangement.SpaceBetween) {
+        ColumnTexts()
+    }
+
+}
+
+/**
+ * [Column]例子使用的布局
+ */
+@Composable
+fun ColumnTexts() {
+    Text(
+        text = "Column1",
+        Modifier
+            .background(c_8BC34A)
+            .padding(4.dp)
+    )
+    Text(
+        text = "Column2",
+        Modifier
+            .background(c_9CCC65)
+            .padding(4.dp)
+    )
+    Text(
+        text = "Column3",
+        Modifier
+            .background(c_AED581)
+            .padding(4.dp)
+    )
+}
+
+/**
+ * [padding]的例子
+ */
+@Composable
+fun ColumnAndRowPaddingExample() {
+
+    val rowModifier = Modifier
+        .background(c_F06292)
+        .fillMaxWidth()
+        .wrapContentHeight()
+
+    //在background之后设置padding (内边距)
+    val modifierA = Modifier
+        .background(c_FFEB3B)
+        .padding(15.dp)
+
+    //在background之前设置padding (外边距,不受padding后面的background的影响)
+    val modifierB = Modifier
+        .padding(10.dp)
+        .background(Color(0xFF80DEEA))
+        .padding(end = 15.dp)
+
+    //先写的属性会先执行,后面的属性会在前面的属性的基础上执行
+    val modifierC = Modifier
+        .background(Color(0xFF607D8B))
+        .padding(15.dp)
+
+    Row(modifier = rowModifier, horizontalArrangement = Arrangement.SpaceEvenly) {
+
+        Column(
+            modifier = modifierA
+                .background(Color.White)
+                .padding(8.dp)
+        )
+        {
+            Text(text = "Text A1")
+            Text(text = "Text A2")
+            Text(text = "Text A3")
+        }
+
+        Column(
+            modifier = modifierB
+                .background(Color(0xFF9575CD))
+                .padding(top = 5.dp, bottom = 30.dp)
+        ) {
+            Text(text = "Text B1")
+            Text(text = "Text B2")
+            Text(text = "Text B3")
+        }
+
+        Column(modifier = modifierC.background(Color(0xFFB2FF59))) {
+            Text(text = "Text C1")
+            Text(text = "Text C2")
+            Text(text = "Text C3")
+        }
+
+    }
+
 }
 
 
--- app/src/main/java/com/linx/jetpack_compose_control_learn/ui/theme/Color.kt
+++ app/src/main/java/com/linx/jetpack_compose_control_learn/ui/theme/Color.kt
@@ -12,3 +12,13 @@ val color_E0 = Color(0xFFE0E0E0)
 val c_FF9800 = Color(0xFFFF9800)
 val c_FFA726 = Color(0xFFFFA726)
 val c_FFB74D = Color(0xFFFFB74D)
+
+
+val c_8BC34A = Color(0xFF8BC34A)
+val c_9CCC65 = Color(0xFF9CCC65)
+val c_AED581 = Color(0xFFAED581)
+
+
+val c_F06292 = Color(0xFFF06292)
+val c_FFEB3B = Color(0xFFFFEB3B)
+val c_80DEEA = Color(0xFF80DEEA)
