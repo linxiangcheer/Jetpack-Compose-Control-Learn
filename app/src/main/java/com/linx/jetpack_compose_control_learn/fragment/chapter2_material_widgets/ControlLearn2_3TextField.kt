d6652ac6567a610d8c4d46895026eb1cfc1a9675
linxiang <1073456077@qq.com>
2021-07-23 18:18:01 +0800

    TextField: colors完善,增加shape、OutlinedTextField、keyboardOptions改变输入类型

--- app/src/main/java/com/linx/jetpack_compose_control_learn/fragment/chapter2_material_widgets/ControlLearn2_3TextField.kt
+++ app/src/main/java/com/linx/jetpack_compose_control_learn/fragment/chapter2_material_widgets/ControlLearn2_3TextField.kt
@@ -1,17 +1,26 @@
 package com.linx.jetpack_compose_control_learn.fragment.chapter2_material_widgets
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import com.linx.jetpack_compose_control_learn.components.ControlLearnDescription
@@ -38,7 +47,7 @@ fun TextFieldScreenContent() {
             //MutableState  TextField和String的不同
             TextFieldOrStringDifferent(fullWidthModifier)
 
-            ControlLearnDescription(text = "2-) isError (true -> 显示错误)")
+            ControlLearnDescription(text = "2-) isError (true -> 提示有错误)")
             ErrorTextField(fullWidthModifier)
 
             ControlLearnDescription(text = "3-) Colors")
@@ -46,23 +55,27 @@ fun TextFieldScreenContent() {
             AllColorsTextFieldExample(fullWidthModifier)
 
             ControlLearnExampleContentText(text = "设置基本颜色属性")
-            ControlLearnExampleContentText(
-                text = "textColor (文本颜色)\nbackgroundColor (背景颜色)\ncursorColor (光标颜色)\n" +
-                        "placeholderColor (提示文字颜色)\nfocusedLabelColor (输入框有焦点时,标签文字颜色)\n" +
-                        "unfocusedLabelColor (输入框不处于焦点时,标签文字颜色)\n" +
-                        "focusedIndicatorColor (输入框处于焦点时,底部指示器的颜色)\n" +
-                        "unfocusedIndicatorColor (输入框不处于焦点时,底部指示器的颜色)"
-            )
             ColorsTextFieldExampleA(fullWidthModifier)
 
-            //todo isError = true 错误时的输入框
-            //todo enabled = false 不允许输入时的输入框
+            ControlLearnExampleContentText(text = " -- isError (true -> 提示有错误)")
+            ColorsErrorTextField(fullWidthModifier)
+
+            ControlLearnExampleContentText(text = " -- Enabled (false -> 禁止输入)")
+            ColorsUnEnabledTextField(fullWidthModifier)
+
+            ControlLearnDescription(text = "4-) Shape (圆角)")
+            ShapeTextField(fullWidthModifier)
+
+            ControlLearnDescription(text = "5-) OutlinedTextField (有轮廓的TextField)")
+            OutlinedTextField(fullWidthModifier)
+
+            ControlLearnDescription(text = "6-) keyboardOptions (改变TextField输入类型)")
+            TypeTextField(fullWidthModifier)
 
         }
     }
 
 }
-
 
 /**
  * 文本框mutableStateOf(TextFieldValue)和mutableStateOf(String)的不同
@@ -209,6 +222,14 @@ private fun AllColorsTextFieldExample(modifier: Modifier) {
 @Composable
 fun ColorsTextFieldExampleA(modifier: Modifier) {
 
+    ControlLearnExampleContentText(
+        text = "textColor (文本颜色)\nbackgroundColor (背景颜色)\ncursorColor (光标颜色)\n" +
+                "placeholderColor (提示文字颜色)\nfocusedLabelColor (输入框有焦点时,标签文字颜色)\n" +
+                "unfocusedLabelColor (输入框不处于焦点时,标签文字颜色)\n" +
+                "focusedIndicatorColor (输入框处于焦点时,底部指示器的颜色)\n" +
+                "unfocusedIndicatorColor (输入框不处于焦点时,底部指示器的颜色)"
+    )
+
     val textFieldValue = remember {
         mutableStateOf(TextFieldValue(""))
     }
@@ -247,6 +268,290 @@ fun ColorsTextFieldExampleA(modifier: Modifier) {
             //当输入框不处于焦点时,底部指示器的颜色
             unfocusedIndicatorColor = Color.White
         ),
+        trailingIcon = {
+            Icon(imageVector = Icons.Default.Close, contentDescription = null)
+        },
+        leadingIcon = {
+            Icon(imageVector = Icons.Default.Favorite, contentDescription = null)
+        }
+    )
+
+}
+
+/**
+ * 提示有错误的时候输入框颜色的改变
+ */
+@Composable
+fun ColorsErrorTextField(modifier: Modifier) {
+
+    ControlLearnExampleContentText(
+        text = "errorLabelColor (isError = true 时label标签文字的颜色)\n" +
+                "errorCursorColor (isError = true 时光标的颜色)\n" +
+                "errorIndicatorColor (isError = true 时底部指示器的颜色)\n" +
+                "errorLeadingIconColor (isError = true 时输入框前头的颜色)\n" +
+                "errorTrailingIconColor (isError = true 时输入框尾部的颜色)"
+    )
+
+    val errorText = remember {
+        mutableStateOf(TextFieldValue(""))
+    }
+    TextField(
+        value = errorText.value,
+        modifier = modifier,
+        onValueChange = { newValue ->
+            errorText.value = newValue
+        },
+        label = {
+            Text(text = "label 标签")
+        },
+        placeholder = {
+            Text(text = "输入文本为空的时候是错误的")
+        },
+        isError = errorText.value.text.isNotEmpty(),
+        colors = TextFieldDefaults.textFieldColors(
+            //isError = true 时label标签文字的颜色
+            errorLabelColor = Color(0xFF003371),
+            //isError = true 时光标的颜色
+            errorCursorColor = Color(0xFF560041),
+            //isError = true 时底部指示器的颜色
+            errorIndicatorColor = Color(0xFF801dae),
+            //isError = true 时输入框前头的颜色
+            errorLeadingIconColor = Color(0xFF4c8dae),
+            //isError = true 时输入框尾部的颜色
+            errorTrailingIconColor = Color.Yellow,
+        ),
+        trailingIcon = {
+            Icon(imageVector = Icons.Default.Close, contentDescription = null)
+        },
+        leadingIcon = {
+            Icon(imageVector = Icons.Default.Favorite, contentDescription = null)
+        }
+    )
+}
+
+/**
+ * 不可输入时输入框颜色的改变
+ */
+@Composable
+fun ColorsUnEnabledTextField(modifier: Modifier) {
+
+    ControlLearnExampleContentText(
+        text = "disabledTextColor (输入框禁用时,已有的文字颜色)\n" +
+                "disabledLabelColor (输入框禁用时,Label标签文字的颜色)\n" +
+                "disabledPlaceholderColor (输入框禁用时,Placeholder提示文字的颜色)\n" +
+                "disabledIndicatorColor (输入框禁用时,底部指示器的颜色)\n" +
+                "disabledLeadingIconColor (输入框禁用时,输入框前头的颜色)\n" +
+                "disabledTrailingIconColor (输入框禁用时,输入框尾部的颜色)"
+    )
+
+    val unEnableText = remember {
+        mutableStateOf(TextFieldValue("禁止输入的输入框"))
+    }
+    TextField(
+        value = unEnableText.value,
+        modifier = modifier,
+        onValueChange = { newValue ->
+            unEnableText.value = newValue
+        },
+        label = {
+            Text(text = "label 标签")
+        },
+        placeholder = {
+            Text(text = "呜呜呜我被禁止输入了")
+        },
+        isError = unEnableText.value.text.isNotEmpty(),
+        colors = TextFieldDefaults.textFieldColors(
+            //输入框禁用时,已有的文字颜色 enabled = false
+            disabledTextColor = Color(0xFF007bbb),
+            //输入框禁用时,Label标签文字的颜色 enabled = false
+            disabledLabelColor = Color(0xFFff8936),
+            //输入框禁用时,Placeholder提示文字的颜色 enabled = false
+            disabledPlaceholderColor = Color(0xFF801dae),
+            //输入框禁用时,底部指示器的颜色 enabled = false
+            disabledIndicatorColor = Color(0xFF2edfa3),
+            //输入框禁用时,输入框前头的颜色 enabled = false
+            disabledLeadingIconColor = Color.White,
+            //输入框禁用时,输入框尾部的颜色 enabled = false
+            disabledTrailingIconColor = Color.Red,
+        ),
+        trailingIcon = {
+            Icon(imageVector = Icons.Default.Close, contentDescription = null)
+        },
+        leadingIcon = {
+            Icon(imageVector = Icons.Default.Favorite, contentDescription = null)
+        },
+        //禁止输入
+        enabled = false
+    )
+
+}
+
+/**
+ * 为TextField添加圆角
+ */
+@Composable
+fun ShapeTextField(modifier: Modifier) {
+
+    val shapeText = remember {
+        mutableStateOf(TextFieldValue("这是一个圆角的TextField"))
+    }
+
+    Surface(
+        modifier = modifier,
+        shape = RoundedCornerShape(25),
+        elevation = 5.dp,
+        //边缘线
+        border = BorderStroke(1.dp, Color.Blue)
+    ) {
+        TextField(
+            value = shapeText.value,
+            onValueChange = { newValue ->
+                shapeText.value = newValue
+            },
+            placeholder = {
+                Text(text = "搜索")
+            },
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = Color.White
+            )
+        )
+    }
+
+}
+
+/**
+ * 有轮廓的TextField
+ */
+@Composable
+fun OutlinedTextField(modifier: Modifier) {
+
+    val textFieldValue = remember {
+        mutableStateOf(TextFieldValue(""))
+    }
+
+    OutlinedTextField(
+        value = textFieldValue.value,
+        onValueChange = { newValue ->
+            textFieldValue.value = newValue
+        },
+        modifier = modifier,
+        placeholder = {
+            Text(text = "这是一个Placeholder")
+        },
+        label = {
+            Text(text = "这是一个Label")
+        }
+    )
+
+    ControlLearnExampleContentText(text = "设置各种颜色")
+    OutlinedTextField(
+        value = textFieldValue.value,
+        onValueChange = { newValue ->
+            textFieldValue.value = newValue
+        },
+        modifier = modifier,
+        placeholder = {
+            Text(text = "这是一个Placeholder")
+        },
+        label = {
+            Text(text = "这是一个Label")
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            textColor = Color.Blue,
+            backgroundColor = Color.Yellow,
+            placeholderColor = Color(0xffFFF176),
+            unfocusedLabelColor = Color(0xff43A047),
+            focusedLabelColor = Color(0xff66BB6A),
+            errorLabelColor = Color(0xffFFEB3B),
+            unfocusedIndicatorColor = Color(0xffFF5722),
+            focusedIndicatorColor = Color(0xff1976D2)
+        )
+    )
+
+    ControlLearnExampleContentText(text = "singleLine (单行)")
+    OutlinedTextField(
+        value = textFieldValue.value,
+        onValueChange = { newValue ->
+            textFieldValue.value = newValue
+        },
+        modifier = modifier,
+        placeholder = {
+            Text(text = "singleLine = true")
+        },
+        label = {
+            Text(text = "singleLine = true")
+        },
+        //单行
+        singleLine = true
+    )
+
+    ControlLearnExampleContentText(text = "maxLines (最大行数)")
+    OutlinedTextField(
+        value = textFieldValue.value,
+        onValueChange = { newValue ->
+            textFieldValue.value = newValue
+        },
+        modifier = modifier,
+        placeholder = {
+            Text(text = "maxLines = 2")
+        },
+        label = {
+            Text(text = "maxLines = 2")
+        },
+        //最大行数
+        maxLines = 2
+    )
+
+
+}
+
+/**
+ * 改变TextField的输入类型
+ */
+@Composable
+fun TypeTextField(modifier: Modifier) {
+
+    ControlLearnExampleContentText(
+        text = "KeyboardType.Password (输入框的类型是密码)\n" +
+                "visualTransformation = PasswordVisualTransformation() (转化为不可见的密码)"
+    )
+
+    val textFieldValue = remember {
+        mutableStateOf(TextFieldValue(""))
+    }
+
+    OutlinedTextField(
+        value = textFieldValue.value,
+        onValueChange = { newValue ->
+            textFieldValue.value = newValue
+        },
+        modifier = modifier,
+        placeholder = {
+            Text(text = "KeyboardType.Password")
+        },
+        label = {
+            Text(text = "请输入密码")
+        },
+        //输入设置  密码
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        //转化为不可见的密码
+        visualTransformation = PasswordVisualTransformation()
+    )
+
+    OutlinedTextField(
+        value = textFieldValue.value,
+        onValueChange = { newValue ->
+            textFieldValue.value = newValue
+        },
+        modifier = modifier,
+        placeholder = {
+            Text(text = "135xxxxxxxx")
+        },
+        label = {
+            Text(text = "请输入手机号")
+        },
+        //输入设置  手机号
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
     )
 
 }
